2091ad96079f80bcbeaeb006b560e54001f13f2a
Kishanr707 <kishanramesh136@gmail.com>
2026-02-12 06:20:41 +0000

    ..

--- 13 linear search.py
+++ 13 linear search.py
@@ -1,1 +1,10 @@
-n = list(list)
+n = list(map(int, input("Enter the number: ").split()))
+x = int(input("Enter the element: "))
+result = -1
+for i in range(len(n)):
+    if (n[i] == x):
+        result = i
+if result == -1:
+    print("Element not found in the array.")
+else:
+    print(f"Element found at index: {result}")
